0cd5d51d4e00d7f0c1dfe73760bcc56a8c58970f
JMerhi01 <132056984+JMerhi01@users.noreply.github.com>
2023-05-03 16:56:12 +0100

    Final

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python Tasks README
+This is a README file for my python tasks at sparta, it will be updated with anything of significance. 
---
+++ improved_movie_ratings.py
@@ -0,0 +1,20 @@
+# New movie ratings
+
+age = input("Please enter your age: ")
+
+# Age check if 1-117
+while not age.isdigit() or int(age) < 1 or int(age) > 117:
+    age = input("Please input an age between 1 and 117: ")
+
+age = int(age)
+
+if age < 12:
+    print("You can only watch movies rated Universal")
+elif age < 15:
+    print("You can watch movies rated Universal and PG")
+elif age < 18:
+    print("You can watch movies rated Universal, PG, and 12")
+elif age == 18:
+    print("You can watch movies rated Universal, PG, 12, and 15, and 18")
+else:
+    print("You can watch movies rated Universal, PG, 12, 15, and 18")
