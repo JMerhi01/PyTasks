1323985397d8449c510d1a1b1f8e4bf9c76b658f
JMerhi01 <132056984+JMerhi01@users.noreply.github.com>
2023-05-04 16:58:56 +0100

    Added c m and ft functions.

--- functional_calculator.py
+++ functional_calculator.py
@@ -16,6 +16,12 @@ def divide(num1, num2):
 def modulo(num1, num2):
     return num1 % num2
 
+def cm_to_m(num):
+    return num / 100
+
+def m_to_feet(num):
+    return num * 3.28084
+
 # Displaying the menu
 print("Select what you'd like to do.")
 print("1.Add")
@@ -23,12 +29,14 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 print("5.Modulo")
+print("6. Convert cm to m")
+print("7. Convert m to feet")
 
 # The Input
 while True:
-    choice = input("Enter choice(1/2/3/4/5): ")
+    choice = input("Enter choice(1/2/3/4//6/7): ")
 # Float to convert input for numbers in to floating numbers
-    if choice in ('1', '2', '3', '4', '5'):
+    if choice in ('1', '2', '3', '4', '5',):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -44,7 +52,14 @@ while True:
         print(num1, "/", num2, "=", divide(num1, num2))
     elif choice == '5':
         print(num1, "%", num2, "=", modulo(num1, num2))
-
+    elif choice == '6':
+        cm = float(input("Enter length in centimeters: "))
+        m = cm_to_m(cm)
+        print(cm, "cm =", m, "m")
+    elif choice == '7':
+        m = float(input("Enter length in meters: "))
+        feet = m_to_feet(m)
+        print(m, "m =", feet, "ft")
 # picked choice with num1 and num2 and print statement.
 # If they misstype something then make an error message
     else:
